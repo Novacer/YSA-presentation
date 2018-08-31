465a61b27ab38b45c1598457da095b643052d208
Jackamac-attack <zhangjack27@gmail.com>
2018-08-31 16:22:21 -0700

    implemented basic frontend

--- example-ui/angular-frontend/src/app/app.component.ts
+++ example-ui/angular-frontend/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,54 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+	
+	public genders : string[];
+	public selectedClass : number;
+	public age: number;
+	public siblings : number;
+	public parents : number;
+	public fare : number;
+	
+	public survived : boolean;
+	
+	private selectedGender : number;
+	
+	constructor(private http: HttpClient) {
+		this.genders = ["Male", "Female"];
+		
+		this.selectedGender = 0;
+		
+		this.selectedClass = 1;
+		this.age = 1;
+		this.siblings = 0;
+		this.parents = 2;
+		this.fare = 100;
+		
+		this.survived = null;
+	}
+	
+	
+	onGenderClick(index: number) {
+		this.selectedGender = index;
+	}
+	
+	onSubmitClick() {
+		let body = {
+			pclass : this.selectedClass,
+			sex : this.selectedGender,
+			age : this.age,
+			siblings : this.siblings,
+			parents : this.parents,
+			fare_price : this.fare
+		};
+		
+		let headers = new HttpHeaders({ 'Content-Type': 'application/json' });
+
+		
+		this.http.post("/api/post/survived-titanic", body, {headers: headers}).subscribe((response : any) => {
+			if (response) {
+				this.survived = response.survived;
+			}
+		});
+	}
 }
