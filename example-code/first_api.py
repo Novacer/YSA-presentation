34d9f37bf71369055aa7c472d42cf359fdf3942c
Jackamac-attack <zhangjack27@gmail.com>
2018-09-01 13:02:44 -0700

    set first_api to a clean slate

--- example-code/first_api.py
+++ example-code/first_api.py
@@ -10,18 +10,12 @@ import requests
 from .machine_learning.titanic import guess_if_survived
 
 
-app = Flask(__name__)
 
-
-# Basic HTTP GET endpoint
-@app.route("/api/get/name/<name>", methods = ['GET'])
 def say_hello(name):
 	return "Hello, " + name
 
+	
 
-# Another HTTP GET endpoint that connects to the openweathermap API to fetch the weather
-# Returns data in JSON format 
-@app.route("/api/get/weather/<city>", methods = ['GET'])
 def get_weather(city):
 	weather = requests.get("http://api.openweathermap.org/data/2.5/weather?q=%s&APPID=API_KEY_HERE" % city)
 	
@@ -32,11 +26,9 @@ def get_weather(city):
 	
 	response = {'the_weather' : weather_description}
 	
-	return jsonify(response)
+	return response
 
 
-# Basic HTTP POST endpoint. Try this one out in Postman!
-@app.route("/api/post/login", methods=['POST'])
 def fake_login():
 	login_attempt = request.get_json()
 	
@@ -54,9 +46,7 @@ def fake_login():
 		
 	return jsonify(response)
 	
-	
-# Machine Learning POST endpoint example
-@app.route("/api/post/survived-titanic", methods=['POST'])
+
 def survived_titanic():
 	features = request.get_json()
 	
